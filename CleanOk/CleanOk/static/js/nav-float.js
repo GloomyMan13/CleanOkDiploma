c7b299b253edc67d6528e0b5eb8030872823833e
GloomyMan13 <darthvaider13@gmail.com>
2021-11-17 23:42:39 +1000

    rewrite main page

--- CleanOk/CleanOk/static/js/nav-float.js
+++ CleanOk/CleanOk/static/js/nav-float.js
@@ -1,12 +1,15 @@
-let floatNav = document.querySelector('.header__container-float');
+let floatNav = document.querySelector('.header__container');
+let floatBtn = document.querySelector('.intro__btn-float');
 window.addEventListener('scroll', function (e) {
     e.preventDefault();
 	let scrollCount = document.documentElement.scrollTop;
     if (scrollCount > 60) {
-        floatNav.classList.remove('hidden');
+        floatNav.classList.add('fixed');
+        floatBtn.classList.remove('hidden');
     } else {
-        floatNav.classList.add('hidden');
+        floatNav.classList.remove('fixed');
+        floatBtn.classList.add('hidden');
     }
-   floatNav.getAnimations({
+floatNav.getAnimations({
                         behavior: 'smooth' });
         });
