2e65f1dce8a41922a8e675f308820bae88f6e55f
CyberEvil777 <danilinb01@gmail.com>
2021-11-18 06:57:20 +0300

    Clean Commit

--- CleanOk/CleanOk/settings/base.py
+++ CleanOk/CleanOk/settings/base.py
@@ -31,7 +31,6 @@ INSTALLED_APPS = [
     'contact',
     'forms',
     'map',
-    'wagtailmap',
 
     'wagtail.contrib.forms',
     'wagtail.contrib.redirects',
@@ -58,7 +57,6 @@ INSTALLED_APPS = [
     'captcha',
     'wagtailcaptcha',
     'wagtailfontawesome',
-    'wagtail_blocks',
 ]
 
 MIDDLEWARE = [
@@ -170,7 +168,7 @@ WAGTAIL_SITE_NAME = "CleanOk"
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
-BASE_URL = 'http://2380-92-37-229-250.ngrok.io'
+BASE_URL = 'http://8444-92-37-249-88.ngrok.io'
 
 # Ключи для капчи
 RECAPTCHA_PUBLIC_KEY = "6Le29RYdAAAAAG3NjbHT0KLcu9c2s68tETqJTPWj"
--- CleanOk/map/urls.py
+++
@@ -1,9 +0,0 @@
-# from django.urls import path
-# from .views import *
-#
-# app_name = 'map'
-# urlpatterns = [
-#     path('geo', calculate_distance_view, name=' calculate_distance_view'),
-# ]
-from .models import MapsDjangoGeoMap
-
